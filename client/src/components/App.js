8dfce7fa8d0d2d5cbdf7c9e074dc189237e7fcf9
Sam Hodak <slhodak@gmail.com>
2019-06-26 15:39:57 -0700

    Render time series chart

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { createChart } from '../timeseries';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
+  componentDidMount() {
+    createChart();
+  }
+  
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <h1>Crypto Viewer</h1>
+        <canvas id="myChart">
+          <p>Fallback content</p>
+        </canvas>
       </div>
     );
   }
