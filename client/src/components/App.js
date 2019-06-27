be973e327c81524031053c1facba2fc108befffb
Sam Hodak <slhodak@gmail.com>
2019-06-26 19:58:56 -0700

    Add currency switching

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,13 +7,13 @@ export default class App extends React.Component {
     this.state = {
       type: 'line',
       types: ['line', 'bar'],
+      currency: 'USD',
       currencyData: []
     };
 
     this.componentDidMount = this.componentDidMount.bind(this);
     this.queryCrypto = this.queryCrypto.bind(this);
-    this.selectType = this.selectType.bind(this);
-    this.updateChart = this.updateChart.bind(this);
+    this.selectOption = this.selectOption.bind(this);
   }
 
   componentDidMount() {
@@ -21,29 +21,30 @@ export default class App extends React.Component {
   }
 
   queryCrypto() {
-    const { type } = this.state;
-    fetch(`http://localhost:3000/prices`)
+    const { currency } = this.state;
+    fetch(`http://localhost:3000/prices?curr=${currency}`)
       .then(response => response.json())
       .then(data => {
         this.setState({
           currencyData: data
         }, () => {
-          this.chart = createChart(data, type);
+          this.chart = createChart(data, 'line', currency);
         });
       })
       .catch(err => console.log(err));
   }
 
-  selectType(event) {
-    this.setState({
-      type: event.target.value
-    });
-  }
-
-  updateChart() {
-    const { type } = this.state;
-    this.chart.config.type = type;
-    this.chart.update();
+  selectOption(event) {
+    if (event.target.name === 'currency') {
+      this.setState({
+        currency: event.target.value
+      }, () => {
+        this.queryCrypto();
+      });
+    } else if (event.target.name === 'type') {
+      this.chart.config.type = event.target.value;
+      this.chart.update();
+    }
   }
 
   render() {
@@ -51,11 +52,14 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>Crypto Viewer</h1>
-        <select name="chartType" onChange={this.selectType}>
+        <select name="type" onChange={this.selectOption}>
           {types.map(type => <option value={type}>{type}</option>)}
         </select>
-        <button type="button" onClick={this.updateChart}>Update</button>
-        <canvas id="myChart">
+        <select name="currency" onChange={this.selectOption}>
+          <option value="USD">USD</option>
+          <option value="CNY">CNY</option>
+        </select>
+        <canvas id="cryptoPrices">
           <p>Fallback content</p>
         </canvas>
         <p>Powered by Coindesk</p>
